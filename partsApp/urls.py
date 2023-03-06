4d63fd6193bd4625d2b7f181e40d268ba16d9a3f
Dawo252 <Dawo252@wp.pl>
2023-03-06 14:17:55 +0100

    working_on_cart

--- partsApp/urls.py
+++ partsApp/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
-    path('part_info', views.part_info, name='part_info'),
+    # path('part_info', views.part_info, name='part_info'),
     path('register/', views.register, name='register'),
-    path('part_info/', views.part_info, name='part_info'),
+    # path('part_info/', views.part_info, name='part_info'),
+    path('cart/', views.cart, name='cart'),
 ]
--- partsApp/views.py
+++ partsApp/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView
 from django.shortcuts import render, redirect
@@ -29,15 +31,41 @@ def index(request):
             return render(request, 'index2.html', {'queryset': object_list})
     else:
         form = SearchParts()
+        ### wazne do koszyka, i ogolnie do zapisywania sesji przy przenoszeniu na strone
+    if request.method == 'POST' and request.POST.get('add_to_cart'):
+        input_value = request.session['part_selected']
+        data = Part.objects.filter(part_name=input_value)
+        if 'cart_list' not in request.session or not request.session['cart_list']:
+            request.session['cart_list'] = [input_value]
+        else:
+            request.session['cart_list'].append(input_value)
+        logging.warning(request.session['cart_list'])
+        request.session.modified = True
     if request.method == 'POST':
-        request.session['part_selected'] = request.POST.get('input_value')
+        input_value = request.session['part_selected']
+        data = Part.objects.filter(part_name=input_value)
+        request.session['part_selected'] = request.POST.get('part_selected')
+        return render(request, 'part_info.html', {'queryset': data})
     return render(request, 'index.html', {'form': form})
 
 
-def part_info(request):
-    input_value = request.session['part_selected']
-    data = Part.objects.filter(part_name=input_value)
-    return render(request, 'part_info.html', {'queryset': data})
+# def part_info(request):
+#     input_value = request.session['part_selected']
+#     data = Part.objects.filter(part_name=input_value)
+#     if request.method == 'POST':
+#         if 'cart_list' not in request.session or not request.session['cart_list']:
+#             request.session['cart_list'] = [input_value]
+#         else:
+#             request.session['cart_list'].append(input_value)
+#         logging.warning(request.session['cart_list'])
+#     request.session.modified = True
+#     return render(request, 'part_info.html', {'queryset': data})
+
+
+def cart(request):
+    cart_list = request.session['cart_list']
+    request.session.modified = True
+    return render(request, 'show_cart.html', {'cart_list': cart_list})
 
 
 def login_view(request):
