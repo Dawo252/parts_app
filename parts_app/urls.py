e2998ade3d0e51e3347f7a2a3e2d00cca3b6e246
Dawo252 <Dawo252@wp.pl>
2023-05-04 11:25:25 +0200

    changed urls to work with media

--- parts_app/urls.py
+++ parts_app/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('partsApp/', include('partsApp.urls')),
-]
+    path('partsApp/', include('partsApp.urls', namespace='partsApp')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
